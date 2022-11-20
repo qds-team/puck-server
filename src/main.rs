6c3fa36b30cf4f0135e526130e454a8645a0b647
tspython <tspython@hotmail.com>
2022-11-19 18:03:25 -0800

    creating endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
 #[macro_use] extern crate rocket;
-
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
-}
+use rocket::fs::FileServer;
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    //rocket::build().mount("/public", FileServer::from("/static"))
+}
+
+#[get("/setPath")]
+fn set_file_path(){
+    /*send list of comic links*/
+}
+
+#[get("/receiveComicName")]
+fn res_comic_name(){
+    /*serve comic file*/
 }
