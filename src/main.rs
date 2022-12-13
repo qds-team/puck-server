785c3343a351b73f1a27ab173c2e5af75dd1d2c9
Tushar Shrivastav <tspython@hotmail.com>
2022-12-12 19:36:51 -0800

    finalze basic API endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,26 @@
 #[macro_use] extern crate rocket;
-use rocket::fs::FileServer;
+use rocket::{get, post, form::Form, routes};
+use rocket_auth::{Users, Error, Auth, Signup, Login};
 
 #[launch]
 fn rocket() -> _ {
-    //rocket::build().mount("/public", FileServer::from("/static"))
+
 }
 
-#[get("/setPath")]
-fn set_file_path(){
-    /*send list of comic links*/
+#[put("/set-path")]
+fn set_file_path() {
+    //TODO: Check if Client is Authenticated
+    //TODO: Set Path of Mangas
 }
 
-#[get("/receiveComicName")]
-fn res_comic_name(){
-    /*serve comic file*/
+#[get("/get-media-list")]
+fn get_media_list() {
+    //TODO: Check if Client is Authenticated
+    //TODO: Return JSON list of Manga Name, and Manga ID's
+}
+
+#[get("/get-media")]
+fn get_media(manga_id: &str) {
+    //TODO: Check if Client is Authenticated
+    //TODO: Implement file server to serve manga file
 }
