aba9dbc090f2176a7dec959c5accb08ca2c3d975
tspython <tspython@hotmail.com>
2023-08-12 18:27:22 -0700

    finished scanning utility :D

--- server/src/utils/scanner.rs
+++ server/src/utils/scanner.rs
@@ -4,15 +4,25 @@ use sqlx::{
 };
 use std::fs;
 use crate::utils::env_load::get_universal_path;
+use crate::routes::scan::ScanErrors;
 
 //#derive[(Debug)]
-enum ScanUtilErrors {
-    DatabaeError(sqlx::Error),
+pub enum ScanUtilErrors {
+    DatabaseError(sqlx::Error),
 }
 
 impl From<sqlx::Error> for ScanUtilErrors {
     fn from(e: sqlx::Error) -> Self {
         ScanUtilErrors::DatabaseError(e)
+    }
+}
+
+impl From<ScanUtilErrors> for ScanErrors {
+    fn from(e: ScanUtilErrors) -> Self {
+        match e {
+            ScanUtilErrors::DatabaseError(sqlx_err) =>
+                Self::SqlxError(sqlx_err),
+        }
     }
 }
 pub async fn get_next_manga_id(pool: &SqlitePool) -> Result<i32, ScanUtilErrors> {
@@ -33,14 +43,12 @@ pub async fn add_manga_db(
     manga_id: i32,
     filename: &str
 ) -> Result<(), ScanUtilErrors> {
-    sqlx::query_as(
+    sqlx::query(
         "INSERT INTO manga_files (manga_id, filename) VALUES ($1, $2)",
     )
     .bind(manga_id)
     .bind(filename)
-    .fetch_one(&mut pool.acquire().await?)
-    .await
-    .map_err(|err| ScanUtilErrors::DatabaseError(err))?;
+    .execute(pool).await?;
 
     Ok(())
 }
@@ -49,12 +57,19 @@ pub async fn scan_dir(pool: &SqlitePool) -> Result<(), ScanUtilErrors> {
     let path = get_universal_path();
 
     // Read the directory
-    let entries = fs::read_dir(path)?;
+    //let entries = fs::read_dir(path)?;
+    let entries = match fs::read_dir(path) {
+        Ok(entries) => entries,
+        Err(io_err) => return Err(ScanUtilErrors::DatabaseError(sqlx::Error::Io(io_err))),
+    };
 
     let id = get_next_manga_id(pool).await?;
 
     for entry in entries {
-        let entry = entry?;
+        let entry = match entry {
+            Ok(entry) => entry,
+            Err(io_err) => return Err(ScanUtilErrors::DatabaseError(sqlx::Error::Io(io_err))),
+        };
         let file_name = entry.file_name();
         let file_name = file_name.to_str().unwrap();
 
